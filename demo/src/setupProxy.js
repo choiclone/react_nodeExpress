f30ba224b1ecbc15203f040e7d74487ba4fc3997
choiclone <s207040@gmail.com>
2022-05-20 11:48:35 +0900

    vscode setting

--- demo/src/setupProxy.js
+++ demo/src/setupProxy.js
@@ -2,11 +2,11 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
   app.use(
-    createProxyMiddleware('/api', { target: `http://172.30.1.18:3000` })
+    createProxyMiddleware('/api', { target: `http://172.30.12.18:3000` })
   );
   app.use(
     createProxyMiddleware('/statics', {
-      target: `http://172.30.1.18:3000`,
+      target: `http://172.30.12.18:3000`,
     })
   );
 };
