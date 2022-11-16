c447340a44f3974a770487a468cce3b1b26bde2e
s207040@gmail.com <s207040@gmail.com>
2022-11-16 14:00:02 +0900

    haha

--- demo/src/setupProxy.js
+++ demo/src/setupProxy.js
@@ -3,18 +3,18 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function (app) {
   // 도커 서버 api docker 빌드 시 사용하면 될듯 ex) http://내_PC_IP:6000 으로 알아서 바꿔라
   
+  // app.use(
+  //   createProxyMiddleware('/api', { 
+  //     target: 'http://localhost:3000',
+  //     changeOrigin: true
+  //   })
+  // );
+  // app.use(
+  //   createProxyMiddleware('/staticFolder', {
+  //     target: 'http://localhost:3000/'
+  //   })
+  // );
   app.use(
-    createProxyMiddleware('/api', { 
-      target: 'http://localhost:3000',
-      changeOrigin: true
-    })
-  );
-  app.use(
-    createProxyMiddleware('/staticFolder', {
-      target: 'http://localhost:3000/'
-    })
-  );
-  /*app.use(
     createProxyMiddleware('/api', { 
       target: 'http://172.30.1.80:6000',
       changeOrigin: true
@@ -24,5 +24,5 @@ module.exports = function (app) {
     createProxyMiddleware('/staticFolder', {
       target: 'http://172.30.1.80:6000/'
     })
-  );*/
+  );
 };
